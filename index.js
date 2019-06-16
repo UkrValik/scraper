fa9d5d59a3b47d61917dc633be9f5e5cb3b70891
UkrValik <krasniukevich@gmail.com>
2019-06-16 17:15:47 +0300

    check heroku

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.setWebHook(`${url}/bot${TOKEN}`);
 
 // Just to ping!
 bot.on('message', function onMessage(msg) {
-  	bot.sendMessage(msg.chat.id, msg.toString());
+  	bot.sendMessage(msg.chat.id, JSON.stringify(msg);
 });
 
 // bot.onText('message', (msg) => {
