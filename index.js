e0a800756a50682ab32fe36039c828c6c3cdfa0e
UkrValik <krasniukevich@gmail.com>
2019-06-18 06:31:58 +0300

    перша адекватна версія

--- index.js
+++ index.js
@@ -74,40 +74,40 @@ bot.onText(/тиждень/, async (msg, match) => {
 		resmsg += lessons[0][i].lesson[1] ? '    Викладач: ' + lessons[0][i].lesson[1] + '\n' : '    -\n'
 		resmsg += lessons[0][i].lesson[2] ? '    Аудиторія: ' + lessons[0][i].lesson[2] + '\n' : '    -\n'
 	}
-	resmsg += "       Вівторок\n"
-	for (let i = 0; i < lessons[1].length; ++i) {
-		resmsg += (i + 1).toString() + ') '
-		resmsg += lessons[1][i].lesson[0] ? 'Предмет: ' + lessons[1][i].lesson[0] + '\n' : '-\n'
-		resmsg += lessons[1][i].lesson[1] ? '    Викладач: ' + lessons[1][i].lesson[1] + '\n' : '    -\n'
-		resmsg += lessons[1][i].lesson[2] ? '    Аудиторія: ' + lessons[1][i].lesson[2] + '\n' : '    -\n'
-	}
-	resmsg += "      Середа\n"
-	for (let i = 0; i < lessons[2].length; ++i) {
-		resmsg += (i + 1).toString() + ') '
-		resmsg += lessons[2][i].lesson[0] ? 'Предмет: ' + lessons[2][i].lesson[0] + '\n' : '-\n'
-		resmsg += lessons[2][i].lesson[1] ? '    Викладач: ' + lessons[2][i].lesson[1] + '\n' : '    -\n'
-		resmsg += lessons[2][i].lesson[2] ? '    Аудиторія: ' + lessons[2][i].lesson[2] + '\n' : '    -\n'
-	}
-	let resmsg = "    Четвер\n"
-	for (let i = 0; i < lessons[3].length; ++i) {
-		resmsg += (i + 1).toString() + ') '
-		resmsg += lessons[3][i].lesson[0] ? 'Предмет: ' + lessons[3][i].lesson[0] + '\n' : '-\n'
-		resmsg += lessons[3][i].lesson[1] ? '    Викладач: ' + lessons[3][i].lesson[1] + '\n' : '    -\n'
-		resmsg += lessons[3][i].lesson[2] ? '    Аудиторія: ' + lessons[3][i].lesson[2] + '\n' : '    -\n'
-	}
-	resmsg += "       П'ятниця\n"
-	for (let i = 0; i < lessons[4].length; ++i) {
-		resmsg += (i + 1).toString() + ') '
-		resmsg += lessons[4][i].lesson[0] ? 'Предмет: ' + lessons[4][i].lesson[0] + '\n' : '-\n'
-		resmsg += lessons[4][i].lesson[1] ? '    Викладач: ' + lessons[4][i].lesson[1] + '\n' : '    -\n'
-		resmsg += lessons[4][i].lesson[2] ? '    Аудиторія: ' + lessons[4][i].lesson[2] + '\n' : '    -\n'
-	}
-	resmsg += "      Субота\n"
-	for (let i = 0; i < lessons[5].length; ++i) {
-		resmsg += (i + 1).toString() + ') '
-		resmsg += lessons[5][i].lesson[0] ? 'Предмет: ' + lessons[5][i].lesson[0] + '\n' : '-\n'
-		resmsg += lessons[5][i].lesson[1] ? '    Викладач: ' + lessons[5][i].lesson[1] + '\n' : '    -\n'
-		resmsg += lessons[5][i].lesson[2] ? '    Аудиторія: ' + lessons[5][i].lesson[2] + '\n' : '    -\n'
-	}
+	// resmsg += "       Вівторок\n"
+	// for (let i = 0; i < lessons[1].length; ++i) {
+	// 	resmsg += (i + 1).toString() + ') '
+	// 	resmsg += lessons[1][i].lesson[0] ? 'Предмет: ' + lessons[1][i].lesson[0] + '\n' : '-\n'
+	// 	resmsg += lessons[1][i].lesson[1] ? '    Викладач: ' + lessons[1][i].lesson[1] + '\n' : '    -\n'
+	// 	resmsg += lessons[1][i].lesson[2] ? '    Аудиторія: ' + lessons[1][i].lesson[2] + '\n' : '    -\n'
+	// }
+	// resmsg += "      Середа\n"
+	// for (let i = 0; i < lessons[2].length; ++i) {
+	// 	resmsg += (i + 1).toString() + ') '
+	// 	resmsg += lessons[2][i].lesson[0] ? 'Предмет: ' + lessons[2][i].lesson[0] + '\n' : '-\n'
+	// 	resmsg += lessons[2][i].lesson[1] ? '    Викладач: ' + lessons[2][i].lesson[1] + '\n' : '    -\n'
+	// 	resmsg += lessons[2][i].lesson[2] ? '    Аудиторія: ' + lessons[2][i].lesson[2] + '\n' : '    -\n'
+	// }
+	// let resmsg = "    Четвер\n"
+	// for (let i = 0; i < lessons[3].length; ++i) {
+	// 	resmsg += (i + 1).toString() + ') '
+	// 	resmsg += lessons[3][i].lesson[0] ? 'Предмет: ' + lessons[3][i].lesson[0] + '\n' : '-\n'
+	// 	resmsg += lessons[3][i].lesson[1] ? '    Викладач: ' + lessons[3][i].lesson[1] + '\n' : '    -\n'
+	// 	resmsg += lessons[3][i].lesson[2] ? '    Аудиторія: ' + lessons[3][i].lesson[2] + '\n' : '    -\n'
+	// }
+	// resmsg += "       П'ятниця\n"
+	// for (let i = 0; i < lessons[4].length; ++i) {
+	// 	resmsg += (i + 1).toString() + ') '
+	// 	resmsg += lessons[4][i].lesson[0] ? 'Предмет: ' + lessons[4][i].lesson[0] + '\n' : '-\n'
+	// 	resmsg += lessons[4][i].lesson[1] ? '    Викладач: ' + lessons[4][i].lesson[1] + '\n' : '    -\n'
+	// 	resmsg += lessons[4][i].lesson[2] ? '    Аудиторія: ' + lessons[4][i].lesson[2] + '\n' : '    -\n'
+	// }
+	// resmsg += "      Субота\n"
+	// for (let i = 0; i < lessons[5].length; ++i) {
+	// 	resmsg += (i + 1).toString() + ') '
+	// 	resmsg += lessons[5][i].lesson[0] ? 'Предмет: ' + lessons[5][i].lesson[0] + '\n' : '-\n'
+	// 	resmsg += lessons[5][i].lesson[1] ? '    Викладач: ' + lessons[5][i].lesson[1] + '\n' : '    -\n'
+	// 	resmsg += lessons[5][i].lesson[2] ? '    Аудиторія: ' + lessons[5][i].lesson[2] + '\n' : '    -\n'
+	// }
 	bot.sendMessage(msg.chat.id, resmsg)	
 })
