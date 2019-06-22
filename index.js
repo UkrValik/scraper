6af1690970a5a0747d93cd5cddb932ef152bb8f3
UkrValik <krasniukevich@gmail.com>
2019-06-22 06:59:03 +0300

    fine output

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const TOKEN = process.env.TELEGRAM_TOKEN || '866032024:AAFGQBML2lMv0o5Jb5HoNKj2l5KIgbfj4qA';
+const TOKEN = '815359260:AAEs9sLQO8S6teExJXTg9zCUJbO6mdNr17M';
 const TelegramBot = require('node-telegram-bot-api');
 const Group = require('./src/Group.js')
 const options = {
@@ -10,7 +10,7 @@ const options = {
 };
 
 const url_lessons = encodeURI('http://api.rozklad.org.ua/v2/groups/іп-71/lessons')
-const url = process.env.APP_URL || 'https://rozklad-for-kpi.herokuapp.com:443';
+const url = 'https://rozklad-for-kpi.herokuapp.com:443';
 const bot = new TelegramBot(TOKEN, options);
 
 let group = new Group()
@@ -41,7 +41,7 @@ bot.onText(/сьогодні/, async (msg, match) => {
 		resmsg += lessons[i][1] ? '    Викладач: ' + lessons[i][1] + '\n' : '    -\n'
 		resmsg += lessons[i][2] ? '    Аудиторія: ' + lessons[i][2] + '\n' : '    -\n'
 	}
-	if (resmsg == "") {
+	if (resmsg == "-\n-\n-\n") {
 		resmsg = "Ви вказали невірний номер групи."
 	}
 	bot.sendMessage(msg.chat.id, resmsg)
